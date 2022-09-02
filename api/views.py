4dd45b9be49a524ed7af15976efeff95b449974a
Paola Roa <paola.roa@correounivalle.edu.co>
2022-09-01 20:42:24 -0500

    commmit 3

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from models import User
+
 
 # Create your views here.
+class UserView(View):
+
+    def get(self, request):
+        users = User.objects.all()
+        if len(users) > 0:
+            datos = {'message': "Success", 'users': users}
+        else:
+            datos = {'message': "Users no found ..."}
+        return JsonResponse(datos)
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
